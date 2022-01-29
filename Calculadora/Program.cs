73f45a6c65e9a15022ddaba104b871aeadc2d710
Jordana1995 <68743811+Jordana1995@users.noreply.github.com>
2022-01-29 11:09:23 -0300

    Polimorfismo

---
+++ Calculadora/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Calculadora.Models;
+
+namespace Calculadora
+{
+
+    class Program
+    {
+
+        static void Main(string[]args)
+        {
+            Calculador calc = new Calculador();
+
+            System.Console.WriteLine("Resultado da primeira soma: " + calc.Somar(10,10));
+            System.Console.WriteLine("Resultado da segunda soma: " + calc.Somar(10,10,10));
+        }
+    }
+}
